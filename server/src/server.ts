fef175025c55e1535fc0977b3b091829600ac054
Renata Karonlina <renatakarolinarko@gmail.com>
2023-05-21 21:36:55 -0400

    aula 1 e aula 2

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import cors from '@fastify/cors'
+import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: ['http://localhost:3333'],
+})
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
